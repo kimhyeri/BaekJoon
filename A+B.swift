076ae75d1d30e3374a2719dc7ae65e2fe3582e2e
kimhyeri <hyer1k@naver.com>
2018-08-06 02:57:58 +0900

    #2741, #1000

---
+++ A+B.swift
@@ -0,0 +1,15 @@
+//
+//  A+B.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 8. 6..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+print(lineArr[0]!+lineArr[1]!)
---
+++ N.swift
@@ -0,0 +1,14 @@
+//
+//  N.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 8. 6..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+for i in (1...a) {
+    print(i)
+}
