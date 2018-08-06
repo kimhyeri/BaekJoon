2653cb5fc2f3fab429ee159ead33f127dc249621
kimhyeri <hyer1k@naver.com>
2018-08-06 16:45:07 +0900

    #2739

---
+++ gugudan.swift
@@ -0,0 +1,13 @@
+//
+//  gugudan.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 8. 6..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+let a = Int(readLine()!)!
+for i in (1...9) {
+    print("\(a) * \(i) = \(a*i)" )
+}
