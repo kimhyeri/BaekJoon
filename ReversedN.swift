c03dd6493a370f8c50a68f8781812497bb3e46e2
kimhyeri <hyer1k@naver.com>
2018-09-19 03:55:45 +0900

    #2742

---
+++ ReversedN.swift
@@ -0,0 +1,15 @@
+//
+//  ReversedN.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 9. 19..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+
+for index in stride(from: a, through: 1, by: -1) {
+    print(index)
+}
