90c5c8f1069e138406f13a33ad55e6146b9350b2
kimhyeri <hyer1k@naver.com>
2018-09-19 11:59:11 +0900

    #1152

---
+++ CountWord.swift
@@ -0,0 +1,13 @@
+//
+//  CountWord.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 9. 19..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+let a = readLine()!
+let getA = a.components(separatedBy: " ")
+print(getA.count)
