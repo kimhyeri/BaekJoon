40df59bbd33986d27573eb093148c9247147b6a7
kimhyeri <hyer1k@naver.com>
2018-09-19 12:09:22 +0900

    #11654

---
+++ Ascii.swift
@@ -0,0 +1,12 @@
+//
+//  Ascii.swift
+//  Algo
+//
+//  Created by hyerikim on 2018. 9. 19..
+//  Copyright © 2018년 hyerikim. All rights reserved.
+//
+
+import Foundation
+
+let a = readLine()!
+print(UnicodeScalar("\(a)")!.value)
