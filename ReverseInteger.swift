acb22f934409e898d25272ef5e2223fa648cd41a
hyeri <hyer1k@naver.com>
2019-12-30 19:02:40 +0900

    LeetCode reverseInteger

---
+++ ReverseInteger.swift
@@ -0,0 +1,27 @@
+//
+//  ReverseInteger.swift
+//  Testing
+//
+//  Created by hyeri kim on 30/12/2019.
+//  Copyright © 2019 hyeri kim. All rights reserved.
+//
+
+import Foundation
+
+func reverse(_ x: Int) -> Int {    
+    var value = String(String(x).reversed())
+    
+    if stringValue.hasPrefix("-") {
+        value.removeLast()
+        value.insert("-", at: value.startIndex)
+    } 
+    
+    if let intValue = Int32(value) {
+        return Int(intValue)
+    }
+    return 0
+}   
+
+print(reverse(123))
+print(reverse(-123))
+print(reverse(120))
